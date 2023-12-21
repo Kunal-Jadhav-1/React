c85578bf6afefa7ee50b469922c2377f9c9b4ca7
Kunal Jadhav <jskunal.01@gmail.com>
2023-12-22 00:52:14 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,39 @@
+// //  Using JS
+// const root = document.getElementById("root");
+// root.innerHTML = "<h1> Hello World from JS<h1>";
+
+{
+    /* <div id="parent">
+          <div id="child">
+              <h1> whatever</h1>
+          </div>
+      </div> */
+}
+
+
+
+
+// //Using React
+
+// const element = React.createElement("h1", {}, "Hello World from React");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(element);
+
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", { id: "something" }, "Whatever"),
+        React.createElement("h2", { id: "something2" }, "Whatever2")
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", { id: "something3" }, "Whatever3"),
+        React.createElement("h2", { id: "something4" }, "Whatever5")
+    ])
+]);
+
+console.log(parent);
+// heading is react element i.e js objects
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
